edbd1c4bfa2e89c1b38af31d47d8e7a283f9c842
Sayak <sayakmodak2003@gmail.com>
2024-08-24 21:55:42 +0530

    added WPM and message feat

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const URL = 'https://dummyjson.com/quotes';
 const quoteDisplay = document.querySelector(".quoteDisplay");
 const textarea = document.getElementById("textarea");
+const btn = document.getElementById("btn");
+const msg = document.getElementById("msg");
+const container = document.querySelector("container");
+const para = document.getElementById("para");
 
+let startTime, endTime;
 
 // getting the quotes from API
 const getQuotes = async () => {
@@ -27,7 +32,7 @@ const generateQuote = async () => {
     textarea.value = null;
 }
 
-
+// text ground by user
 textarea.addEventListener("input", () => {
     const charValue = textarea.value.split('');
     // console.log(charValue);
@@ -35,9 +40,9 @@ textarea.addEventListener("input", () => {
     // elm==span 
     let correct = true;
     compareElm.forEach((elm, index) => {
-        console.log(elm.innerText);
+        // console.log(elm.innerText);
         const char = charValue[index];
-        if (char == null) {
+        if (char == undefined) {
             elm.classList.remove("green");
             elm.classList.remove("red");
             correct = false;
@@ -55,18 +60,74 @@ textarea.addEventListener("input", () => {
 
     })
 
-    if (correct) {
-        generateQuote();
-    }
-    // Array.from(compareElm).forEach((elm, index) => {
-    //     console.log(elm.innerText);
-    // })
-    // textarea.value.split("").forEach((elm) => {
-    //     // 
-    // })
-
-
+    // getting the next quote if all right
+    // if (correct) {
+    //     generateQuote();
+    // }
 })
+
+const endTyping = () => {
+    const date = new Date();
+    endTime = date.getTime();
+}
+
+const startTyping = () => {
+    const date = new Date();
+
+
+    if (btn.innerText === "Start") {
+        btn.innerText = "Done";
+        textarea.removeAttribute("disabled");
+        startTime = date.getTime();
+        return;
+    }
+
+    if (btn.innerText === "Done") {
+        btn.innerText = "Start";
+        textarea.setAttribute("disabled", "true");
+        endTyping();
+
+        // const p = document.createElement("p");
+        // p.classList.add("para");
+        // container.appendChild(p);
+
+        let countDown = 5;
+        const interval = setInterval(() => {
+            countDown--;
+
+            para.innerText = `Next quote will be generated within ${countDown} seconds`;
+
+            if (countDown < 1) {
+                para.innerText = "";
+                clearInterval(interval);
+            }
+
+        }, 1000);
+
+        setTimeout(() => {
+            generateQuote();
+            msg.innerText = '';
+        }, 5000);
+
+    }
+
+    totalTimeTaken();
+}
+
+const totalTimeTaken = () => {
+    const time_taken = (endTime - startTime) / 1000;
+    const typedWords = textarea.value.trim();
+    const actual_words = typedWords.split(" ");
+    const WPM = (actual_words.length / time_taken) * 60;
+    msg.innerText = `Total word written ${actual_words.length} and WPM ${Math.round(WPM)}`;
+}
+
+
+btn.addEventListener("click", () => {
+    startTyping();
+})
+
+
 
 generateQuote();
 
